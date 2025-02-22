58f9739fcef33eb5c41f1fdf13fcbe1bcc3cac5e
Eremel <richardbailey@outlook.com>
2025-02-22 20:42:38 +0000

    Fix blacklist double trigger

--- objects/jokers/blacklist.lua
+++ objects/jokers/blacklist.lua
@@ -38,9 +38,6 @@ SMODS.Joker({
                             return true
                         end
                     }))
-                    ease_dollars(card.ability.extra.dollars)
-                    G.GAME.dollar_buffer = (G.GAME.dollar_buffer or 0) + card.ability.extra.dollars
-                    G.E_MANAGER:add_event(Event({func = (function() G.GAME.dollar_buffer = 0; return true end)}))
                     return {
                         dollars = card.ability.extra.dollars,
                         colour = G.C.MONEY
