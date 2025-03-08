5248e9450413fdc2b332788d354d8717265b3824
Eremel <richardbailey@outlook.com>
2025-03-08 22:49:25 +0000

    Clean up

--- objects/jokers/wallet.lua
+++ objects/jokers/wallet.lua
@@ -9,12 +9,7 @@ SMODS.Joker({
     blueprint_compat = false,
     eternal_compat = true,
     perishable_compat = true,
-    config = {extra = {}},
     loc_vars = function(self, info_queue, card)
         if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'eremel'} end
-        return {vars = {}}
-    end,
-    calculate = function(self, card, context)
-        
-    end    
+    end
 })
