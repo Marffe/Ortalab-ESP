1060368ded3bc92c949789c5388297c32fbb9baf
Eremel <richardbailey@outlook.com>
2025-03-11 15:53:42 +0000

    perishable compat

--- objects/jokers/bear.lua
+++ objects/jokers/bear.lua
@@ -8,7 +8,7 @@ SMODS.Joker({
     discovered = false,
     blueprint_compat = true,
     eternal_compat = true,
-    perishable_compat = true,
+    perishable_compat = false,
     config = {extra = {dollars = 20, spent = 0, mult = 0, gain = 3}},
     loc_vars = function(self, info_queue, card)
         if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'no_demo'} end
