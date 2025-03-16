d54f747f8395f7449cb111bdf9fa3cd7dabac3ee
Eremel <richardbailey@outlook.com>
2025-03-16 02:23:15 +0000

    belmont crash fix

--- objects/jokers/belmont.lua
+++ objects/jokers/belmont.lua
@@ -12,7 +12,8 @@ SMODS.Joker({
     config = {extra = {mult = 4}},
     loc_vars = function(self, info_queue, card)
         if card and not card.fake_card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'no_demo'} end
-        local count = Ortalab.curses_in_deck()
+        local count = 0
+        if G.playing_cards then count = Ortalab.curses_in_deck() end
         return {vars = {card.ability.extra.mult, card.ability.extra.mult * count}}
     end,
     calculate = function(self, card, context)
