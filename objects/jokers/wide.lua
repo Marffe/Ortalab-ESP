2bdb55308fe5326f3617c8b09491169be74c03ff
Eremel <richardbailey@outlook.com>
2025-03-25 19:00:40 +0000

    Art update

--- objects/jokers/wide.lua
+++ objects/jokers/wide.lua
@@ -10,8 +10,9 @@ SMODS.Joker({
     eternal_compat = true,
     perishable_compat = false,
     config = {extra = {mult = 4, gain = 2}},
+    -- display_size = {w = 142, h = 50},
     loc_vars = function(self, info_queue, card)
-        if card and not card.fake_card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'no_demo'} end
+        if card and not card.fake_card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'kosze'} end
         return {vars = {card.ability.extra.gain, card.ability.extra.mult}}
     end,
     calculate = function(self, card, context)
@@ -32,3 +33,21 @@ SMODS.Joker({
         end
     end    
 })
+
+local hover = Card.hover
+function Card:hover()
+    hover(self)
+    if self.config.center_key == 'j_ortalab_wide' and self.T.w == G.CARD_W then
+        ease_value(self.T, 'w', self.T.w, nil, nil, nil, nil, 'elastic')
+        ease_value(self.T, 'h', -(self.T.h/2), nil, nil, nil, nil, 'elastic')
+    end
+end
+
+local stop_hover = Card.stop_hover
+function Card:stop_hover()
+    stop_hover(self)
+    if self.config.center_key == 'j_ortalab_wide' and self.T.w > G.CARD_W then
+        ease_value(self.T, 'w', G.CARD_W - self.T.w)
+        ease_value(self.T, 'h', G.CARD_H - self.T.h)
+    end
+end
