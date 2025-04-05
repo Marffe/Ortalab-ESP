4565471936268991f589ed6635944d0a611cc27f
Eremel <richardbailey@outlook.com>
2025-04-05 23:02:16 +0100

    $2 > $3

--- objects/jokers/scam_email.lua
+++ objects/jokers/scam_email.lua
@@ -9,7 +9,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	config = {extra = {dollars = 2, rank = 'Jack'}},
+	config = {extra = {dollars = 3, rank = 'Jack'}},
 	loc_vars = function(self, info_queue, card)
         if card and not card.fake_card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'gappie'} end
 		return {vars = {card.ability.extra.dollars, localize(card.ability.extra.rank, 'ranks')}}
