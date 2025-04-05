65c763ebcde955854e82e2a87a5a1e0ce0b4998d
Eremel <richardbailey@outlook.com>
2025-04-05 23:03:43 +0100

    1.75X > 2X

--- objects/jokers/mint_condition.lua
+++ objects/jokers/mint_condition.lua
@@ -10,7 +10,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	config = {extra = {Xmult = 1.75}},
+	config = {extra = {Xmult = 2}},
 	loc_vars = function(self, info_queue, card)
         if card and not card.fake_card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'hat'} end
         return {vars = {card.ability.extra.Xmult}}
