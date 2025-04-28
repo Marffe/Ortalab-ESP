6cd692163e359cf520f7975fdca9ca70c85cffb3
Eremel <richardbailey@outlook.com>
2025-04-28 23:01:43 +0100

    Fix polydactyly

--- objects/jokers/polydactyly.lua
+++ objects/jokers/polydactyly.lua
@@ -14,4 +14,11 @@ SMODS.Joker({
         if card and not card.fake_card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'eremel'}; info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'grassy'} end
         return {vars = {card.ability.extra.cards, localize(card.ability.extra.hand_1, 'poker_hands'), localize(card.ability.extra.hand_2, 'poker_hands')}}
 	end,
+	calculate = function(self, card, context)
+		if context.after then
+			Ortalab.polydactyly.cards = {}
+		end
+	end
 })
+
+Ortalab.polydactyly = {cards = {}}
