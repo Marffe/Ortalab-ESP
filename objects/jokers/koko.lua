986a714b1333a7046414ae6ae69a534591c3752b
Eremel <richardbailey@outlook.com>
2025-06-07 23:41:00 +0100

    Koko: ophiuchus typo T_T

--- objects/jokers/koko.lua
+++ objects/jokers/koko.lua
@@ -17,7 +17,7 @@ SMODS.Joker({
     end,
     set_ability = function(self, card, initial, delay)
         for k, _ in pairs(G.ZODIACS) do
-            if k ~= 'zodiac_ortalab_ophichus' then
+            if k ~= 'zodiac_ortalab_ophiuchus' then
                 card.ability.extra.zodiacs[#card.ability.extra.zodiacs+1] = k
             end
         end
