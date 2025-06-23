62064ca5a4a75f349dea592de1db6d23fae09e45
SuportSnek <lzmorese@gmail.com>
2025-06-23 17:09:43 -0400

    Now need an Ore card in deck to find Pickaxe

--- objects/jokers/pickaxe.lua
+++ objects/jokers/pickaxe.lua
@@ -11,6 +11,7 @@ SMODS.Joker({
 	perishable_compat = true,
     config = {extra = {xmult = 1, gain = 0.2}},
     artist_credits = {'gappie'},
+    enhancement_gate = 'm_ortalab_ore',
 	loc_vars = function(self, info_queue, card)
         return {vars = {card.ability.extra.xmult, card.ability.extra.gain}}
     end,
