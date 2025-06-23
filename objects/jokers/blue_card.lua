13958848962740ead558b9b5809c946af5acbc73
SuportSnek <lzmorese@gmail.com>
2025-06-23 16:54:31 -0400

    Blue card: Price increase 3->4, chips gained 6->5

--- objects/jokers/blue_card.lua
+++ objects/jokers/blue_card.lua
@@ -3,13 +3,13 @@ SMODS.Joker({
 	atlas = "jokers",
 	pos = {x=2,y=4},
 	rarity = 1,
-	cost = 3,
+	cost = 4,
 	unlocked = true,
 	discovered = false,
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = false,
-	config = {extra = {chips = 0, gain = 6}},
+	config = {extra = {chips = 0, gain = 5}},
 	artist_credits = {'gappie'},
 	loc_vars = function(self, info_queue, card)
 		return {vars = {card.ability.extra.gain, card.ability.extra.chips}}
