fdf256e51541cb003f7d9d5417a012603fa20551
Richard <richardbailey@outlook.com>
2025-06-27 01:48:21 +0100

    Home Delivery: attempt to fix crash, add util function to solve crash and provide more info

--- objects/coupons.lua
+++ objects/coupons.lua
@@ -53,6 +53,50 @@ SMODS.Voucher({
     artist_credits = {'flare'},
 })
 
+local ortalab_skip_blind = G.FUNCS.skip_blind
+G.FUNCS.skip_blind = function(e)
+    Ortalab.queue_size = #G.E_MANAGER.queues.base
+    print('Queue size: '..Ortalab.queue_size)
+    ortalab_skip_blind(e)
+    G.E_MANAGER:add_event(Event({
+    trigger = 'after',
+    func = function()
+        if G.GAME.used_vouchers['v_ortalab_home_delivery'] then
+            if G.blind_select then 
+                G.blind_select:remove()
+                G.blind_prompt_box:remove()
+                G.blind_select = nil
+            end
+            G.GAME.current_round.used_packs = {}
+            G.GAME.current_round.jokers_purchased = 0
+            G.GAME.current_round.reroll_cost_increase = 0
+            G.GAME.round_resets.temp_reroll_cost = nil
+            G.GAME.current_round.free_rerolls = G.GAME.round_resets.free_rerolls
+            calculate_reroll_cost(true)
+
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                blocking = false,
+                func = function()
+                    print('Queue Size: '..Ortalab.queue_size..' Actual size: '..#G.E_MANAGER.queues.base)
+                    if G.STATE == G.STATES.SMODS_BOOSTER_OPENED or #G.E_MANAGER.queues.base > Ortalab.queue_size then return false end
+                    G.STATE_COMPLETE = false
+                    G.STATE = G.STATES.SHOP
+                    return true
+                end
+            }))
+        end
+        return true
+    end}))
+end
+
+function Ortalab.home_delivery()
+    print(inspectFunction(G.E_MANAGER.queues.base[#G.E_MANAGER.queues.base].func))
+    
+    G.STATE_COMPLETE = false
+    G.STATE = G.STATES.SHOP
+end
+
 SMODS.Voucher({
 	key = "hoarding",
 	atlas = "coupons",
