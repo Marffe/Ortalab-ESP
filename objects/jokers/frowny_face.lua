17313ef4f401de18a4a7b76d66bc773aada9a7f9
SuportSnek <lzmorese@gmail.com>
2025-07-16 19:26:47 -0400

    Frowny Face: +4 mult for 2, 3, 4 and 5

--- objects/jokers/frowny_face.lua
+++ objects/jokers/frowny_face.lua
@@ -9,13 +9,13 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	config = {extra = {mult = 3}},
+	config = {extra = {mult = 4}},
 	artist_credits = {'golddisco'},
     loc_vars = function(self, info_queue, card)
         return {vars = {card.ability.extra.mult}}
     end,
 	calculate = function(self, card, context) --Frowny Face Logic
-        if context.individual and context.cardarea == G.play and context.other_card:is_numbered() then
+        if context.individual and context.cardarea == G.play and (context.other_card:get_id() == 2 or context.other_card:get_id() == 3 or context.other_card:get_id() == 4 or context.other_card:get_id() == 5) then
             return {
                 mult = card.ability.extra.mult,
                 card = card
