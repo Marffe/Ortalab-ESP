c3f759c19ec8111eb6ac97335a7299aa24d718b3
Eremel <richardbailey@outlook.com>
2025-07-26 19:37:28 +0100

    Televangelist: remove old code T_T

--- objects/jokers/televangelist.lua
+++ objects/jokers/televangelist.lua
@@ -13,16 +13,5 @@ SMODS.Joker({
 	artist_credits = {'gappie'},
 	loc_vars = function(self, info_queue, card)
 		return {vars = {SMODS.get_probability_vars(card, 1, card.ability.extra.chance)}}
-	end,
-	calculate = function(self, card, context)
-		if context.using_consumeable and context.consumeable.ability.set == 'Loteria' and not context.blueprint then
-			card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.gain
-            SMODS.calculate_effect({message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.xmult}}}, card)
-		end
-        if context.joker_main then
-            return {
-                xmult = card.ability.extra.xmult
-            }
-        end
-    end
+	end
 })
