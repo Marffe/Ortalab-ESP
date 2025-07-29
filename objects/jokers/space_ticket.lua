05ec0acb720adeef74e893499f2c8b2ee456fe7c
SuportSnek <lzmorese@gmail.com>
2025-07-29 17:01:03 -0400

    Space ticket: $5->$4

--- objects/jokers/space_ticket.lua
+++ objects/jokers/space_ticket.lua
@@ -4,7 +4,7 @@ SMODS.Joker({
     atlas = "jokers",
     pos = {x = 8, y = 6},
     rarity = 1,
-    cost = 5,
+    cost = 4,
     unlocked = true,
     discovered = false,
     blueprint_compat = true,
