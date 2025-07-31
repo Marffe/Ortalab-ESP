fc364ac0dff0beb1d861c593f6c0698edbcbce77
Eremel <richardbailey@outlook.com>
2025-07-31 20:49:44 +0100

    Damp Joker: refactor

--- objects/jokers/damp.lua
+++ objects/jokers/damp.lua
@@ -16,11 +16,12 @@ SMODS.Joker({
 			juice_card_until(card, eval, true)
 		end
 		if context.end_of_round and context.main_eval and G.GAME.current_round.hands_played == 1 then
-				card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_upgrade_ex')})
-				update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=localize(G.GAME.last_hand_played, 'poker_hands'),chips = G.GAME.hands[G.GAME.last_hand_played].chips, mult = G.GAME.hands[G.GAME.last_hand_played].mult, level=G.GAME.hands[G.GAME.last_hand_played].level})
-                level_up_hand(context.blueprint_card or card, G.GAME.last_hand_played, nil, 1)
-                update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
-			return nil, true
+			return {
+				message = localize('k_upgrade_ex'),
+				colour = G.C.BLUE,
+				level_up = true,
+				level_up_hand = G.GAME.last_hand_played
+			}
 		end
 	end
 })
