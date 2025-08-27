1a988228a03aa1f394b5030a102f00f4ccca041c
Eremel <richardbailey@outlook.com>
2025-08-28 00:05:04 +0100

    Hand Types that don't have zodiacs no longer crash

--- objects/jokers/pin_point.lua
+++ objects/jokers/pin_point.lua
@@ -12,7 +12,7 @@ SMODS.Joker({
 	artist_credits = {'gappie'},
 	calculate = function(self, card, context)
         if context.end_of_round and not context.repetition and not context.individual and not context.blueprint and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-            if G.GAME.last_hand_played then
+            if G.GAME.last_hand_played and zodiac_from_hand(G.GAME.last_hand_played) then
                 local zodiac_key = zodiac_from_hand(G.GAME.last_hand_played)
                 local new_zodiac = create_card('ortalab_zodiac', G.consumeables, nil, nil, true,  true, zodiac_key)
                 new_zodiac:add_to_deck()
