2df6cc1ffe0d5bf892e6138740a48c1448ed9f26
Eremel <richardbailey@outlook.com>
2025-08-27 23:59:51 +0100

    Polydactyly: stop duplicate triggers

--- objects/jokers/polydactyly.lua
+++ objects/jokers/polydactyly.lua
@@ -22,3 +22,25 @@ SMODS.Joker({
 })
 
 Ortalab.polydactyly = {cards = {}}
+
+local ortalab_calc_main_scoring = SMODS.calculate_main_scoring
+function SMODS.calculate_main_scoring(context, scoring_hand)
+	ortalab_calc_main_scoring(context, scoring_hand)
+	if context.cardarea == G.play then
+		context.cardarea = Ortalab.polydactyly
+		ortalab_calc_main_scoring(context, scoring_hand)
+		context.cardarea = G.play
+	end
+	return
+end
+
+local ortalab_calc_destroyed = SMODS.calculate_destroying_cards
+function SMODS.calculate_destroying_cards(context, cards_destroyed, scoring_hand)
+	ortalab_calc_destroyed(context, scoring_hand)
+	if context.cardarea == G.play then
+		context.cardarea = Ortalab.polydactyly
+		ortalab_calc_destroyed(context, cards_destroyed, scoring_hand)
+		context.cardarea = G.play
+	end
+	return
+end
