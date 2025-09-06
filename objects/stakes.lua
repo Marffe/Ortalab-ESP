4ef87e3833a8e149f2a69c73db4c56e812a72eb3
Eremel <richardbailey@outlook.com>
2025-09-06 22:01:07 +0100

    Some code clean up

--- objects/stakes.lua
+++ objects/stakes.lua
@@ -235,39 +235,6 @@ SMODS.Stake({
     end,
 })
 
-function get_pack(_key, _type)
-    if not G.GAME.first_shop_buffoon and not G.GAME.banned_keys['p_buffoon_normal_1'] then
-        G.GAME.first_shop_buffoon = true
-        return G.P_CENTERS['p_buffoon_normal_'..(math.random(1, 2))]
-    end
-    local cume, it, center = 0, 0, nil
-	local temp_in_pool = {}
-    for k, v in ipairs(G.P_CENTER_POOLS['Booster']) do
-		local add
-		v.current_weight = v.get_weight and v:get_weight() or v.weight or 1
-        if (not _type or _type == v.kind) then add = true end
-		if v.in_pool and type(v.in_pool) == 'function' then 
-			local res, pool_opts = v:in_pool()
-			pool_opts = pool_opts or {}
-			add = res and (add or pool_opts.override_base_checks)
-		end
-        if v.kind ~= 'Buffoon' and v.kind ~= 'Standard' and G.GAME.modifiers.ortalab_only then
-            if not v.mod or v.mod.id ~= 'ortalab' then add = false end
-        end
-		if add and not G.GAME.banned_keys[v.key] then cume = cume + (v.current_weight or 1); temp_in_pool[v.key] = true end
-    end
-    local poll = pseudorandom(pseudoseed((_key or 'pack_generic')..G.GAME.round_resets.ante))*cume
-    for k, v in ipairs(G.P_CENTER_POOLS['Booster']) do
-        if temp_in_pool[v.key] then 
-            it = it + (v.current_weight or 1)
-            if it >= poll and it - (v.current_weight or 1) <= poll then center = v; break end
-        end
-    end
-   if not center then center = G.P_CENTERS['p_buffoon_normal_1'] end  return center
-end
-
-
-
 -- Override get_next_tag_key to disable skipping the final ante blinds
 local get_next_tag_key_ortalab_ref = get_next_tag_key
 function get_next_tag_key(append)
